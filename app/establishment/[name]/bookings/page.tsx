de9952a3a8cf5ed0831b1db6909062f60667e592
Aniel <anielqsilva@gmail.com>
2024-09-02 17:45:12 -0300

    feat: created custom bookings page

--- app/establishment/[name]/bookings/page.tsx
+++ app/establishment/[name]/bookings/page.tsx
@@ -1,15 +1,85 @@
-import { getEstablishment } from "@/app/_actions/get-establishment";
-import { notFound } from "next/navigation";
+import { getServerSession } from "next-auth"
+import Header from "@/app/_components/header"
+import { db } from "@/app/_lib/prisma"
+import { authOptions } from "@/app/_lib/auth"
+import SignInDialogClientWrapper from "@/app/_components/sign-in-wrapper"
+import BookingItem from "@/app/_components/booking-item"
 
-const BookingsPage = async ({ params } :  { params: { name: string}}) => {
-    const { name } = params
-    const establishment = await getEstablishment({ subdomain: name })
+const Bookings = async () => {
+  const session = await getServerSession(authOptions)
+  let concludedBookings: any[] = []
+  let confirmedBookings: any[] = []
 
-    if (!establishment) return notFound()
+  if (session && session.user) {
+    confirmedBookings = await db.booking.findMany({
+      where: {
+        userId: (session?.user as any).id,
+        date: {
+          gte: new Date(),
+        },
+      },
+      include: {
+        service: {
+          include: {
+            barbershop: true,
+          },
+        },
+      },
+      orderBy: {
+        date: "asc",
+      },
+    })
 
-    return (
-        <h1>Olá, {name}</h1>
-    );
+    concludedBookings = await db.booking.findMany({
+      where: {
+        userId: (session?.user as any).id,
+        date: {
+          lte: new Date(),
+        },
+      },
+      include: {
+        service: {
+          include: {
+            barbershop: true,
+          },
+        },
+      },
+      orderBy: {
+        date: "asc",
+      },
+    })
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="space-y-3 p-5">
+        {!session?.user && <SignInDialogClientWrapper />}
+
+        {confirmedBookings.length > 0 && (
+          <>
+            <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+              Confirmados
+            </h2>
+            {confirmedBookings.map((booking) => (
+              <BookingItem key={booking.id} booking={booking} />
+            ))}
+          </>
+        )}
+
+        {concludedBookings.length > 0 && (
+          <>
+            <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+              Finalizados
+            </h2>
+            {concludedBookings.map((booking) => (
+              <BookingItem key={booking.id} booking={booking} />
+            ))}
+          </>
+        )}
+      </div>
+    </>
+  )
 }
- 
-export default BookingsPage;
+
+export default Bookings
