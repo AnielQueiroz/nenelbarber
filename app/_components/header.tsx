630ce2685ebaca30b56e40651e813bd319a7e542
Aniel <anielqsilva@gmail.com>
2024-09-02 17:40:07 -0300

    chore: removed link to the default home

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,32 +1,31 @@
-import Image from "next/image";
-import { Card, CardContent } from "./ui/card";
-import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
-import { Sheet, SheetTrigger } from "./ui/sheet";
+import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
+import { MenuIcon } from "lucide-react"
+import { Sheet, SheetTrigger } from "./ui/sheet"
 
-import SidebarSheet from "./sidebar-sheet";
-import Link from "next/link";
+import SidebarSheet from "./sidebar-sheet"
 
 const Header = () => {
-    return ( 
-        <Card>
-            <CardContent className="p-5 flex flex-row justify-between items-center">
-                <Link href="/">                
-                    <Image src={"/logo.png"} width={120} height={18} alt="Nenel Barber"/>
-                </Link>
-                
-                <Sheet>
-                    <SheetTrigger asChild>
-                        <Button size="icon" variant="outline">
-                            <MenuIcon />
-                        </Button>
-                    </SheetTrigger>
-                    
-                    <SidebarSheet />
-                </Sheet>
-            </CardContent>
-        </Card>
-     );
+  return (
+    <Card>
+      <CardContent className="flex flex-row items-center justify-between p-5">
+        {/* <Link href="/">                 */}
+        <Image src={"/logo.png"} width={120} height={18} alt="Nenel Barber" />
+        {/* </Link> */}
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+
+          <SidebarSheet />
+        </Sheet>
+      </CardContent>
+    </Card>
+  )
 }
- 
-export default Header;
+
+export default Header
